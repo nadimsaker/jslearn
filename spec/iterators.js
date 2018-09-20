e4b7e707860f6d838e269e37cfd73ae524c5f64d
Nadim Saker <nadimsaker@gmail.com>
2018-09-20 12:43:21 +0600

    Iterators update

--- spec/iterators.js
+++ spec/iterators.js
@@ -3,11 +3,32 @@ describe("iterables", function(){
     it("can work with iterators at a low level", function(){
 
         let sum = 0;
-        let number = [1,2,3,4];
+        let numbers = [1,2,3,4];
 
-        for(let i = 0; i<number.length;i++){
-            sum+=number[i];
+        //for
+        for(let i = 0; i<numbers.length;i++){
+            sum+=numbers[i];
         }
         expect(sum).toBe(10);
+
+        //for in
+        sum = 0;
+        for(let i in numbers){
+            sum+=numbers[i];
+        }
+        expect(sum).toEqual(10);
+
+        Iterator
+        sum = 0;
+        let iterator = numbers.values();
+        let next = iterator.next();
+
+        while(!next.done){
+            sum += next.value;
+            next = iterator.next();
+        }
+        expect(sum).toBe(10);
+
+       
     });
 });
